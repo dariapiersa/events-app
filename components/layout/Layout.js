fafaccafcf671878fe308044057bab8bf9b24d10
Daria Piersa <dariapiersa17@gmail.com>
2022-09-19 20:26:17 +0200

    triggering & showing notification

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,13 +1,25 @@
-import { Fragment } from "react";
+import { Fragment, useContext } from "react";
 import MainHeader from "./MainHeader";
+import Notification from "../ui/Notification";
+import NotificationContext from "../../store/notification-context";
 
 function Layout(props) {
-    return (
-      <Fragment>
-        <MainHeader />
-        <main>{props.children}</main>
-      </Fragment>
-    );
-  }
-  
-  export default Layout;
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotification = notificationCtx.notification;
+  return (
+    <Fragment>
+      <MainHeader />
+      <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
+    </Fragment>
+  );
+}
+
+export default Layout;
